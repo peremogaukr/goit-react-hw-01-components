722962642d2989307409e771aa4cb6f204da2800
peremogaukr <volovikoi28@gmail.com>
2023-12-09 21:09:25 +0200

    fr list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { UserProfile } from 'components/Profile/Profile';
+import user from 'JSON/user.json';
+
+import { UserStatistic } from 'components/Statistic/Statistic';
+import data from 'JSON/data.json';
+
+import { Friends } from './Friend-list/Friends';
+import friends from 'JSON/friends.json';
+
+import { TransactionHistoryUser } from 'components/Transaction/Transaction';
+import  transaction from 'JSON/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <UserProfile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <UserStatistic title="Statistic" stats={data} />
+      <Friends friends={friends} />
+      <TransactionHistoryUser items={transaction}/>
+    </>
   );
 };
---
+++ src/components/friend-list-item/Friend.list.item.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import css from './Friend-list.module.css';
+
+export const FreindsListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <>
+      <li className={css.item}>
+        <span
+          className={`${css.status} ${isOnline ? css.online : css.offline}`}
+        >
+          {isOnline}
+        </span>
+        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+        <p className={css.name}>{name}</p>
+      </li>
+    </>
+  );
+};
+
+FreindsListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
